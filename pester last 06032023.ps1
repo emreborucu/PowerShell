36ad3c6bc1054beac2ae486017c1255206a17145
ahmetsngr <88079221+ahmetsngr@users.noreply.github.com>
2023-03-07 15:11:59 +0300

    Add files via upload

---
+++ pester last 06032023.ps1
@@ -0,0 +1,23 @@
+﻿Import-Module PSFramework
+Import-Module dbatools
+
+Set-DbcConfig policy.backup.logmaxminutes -Value 1440
+set-DbcConfig policy.backup.fullmaxdays -Value 11520
+set-DbcConfig policy.backup.diffmaxhours -Value 2880
+
+Set-DbcConfig policy.dbcc.maxdays -Value 30 
+
+$categories = "Instance","Server"
+$instance = Get-Content -Path "C:\PSScripts\server-list.txt"
+$HostName=$instance
+
+$HostName= $instance.Replace("\RETAILSTORE","")
+
+
+##$exluded_checks = "FKCKTrusted","DisabledIndex","SymmetricKeyEncryptionLevel","AsymmetricKeySize","LogShipping", "QueryStoreEnabled","SqlBrowserServiceAccount"
+$exluded_checks = "DatabaseGrowthEvent","IdentityUsage","FKCKTrusted","DuplicateIndex","UnusedIndex","DisabledIndex","SymmetricKeyEncryptionLevel","AsymmetricKeySize","MaintenanceSolution","LogShipping", "QueryStoreEnabled","Agent","SqlBrowserServiceAccount"
+
+
+$result = Invoke-DbcCheck -SqlInstance $instance -ComputerName $HostName -AllChecks -ExcludeCheck $exluded_checks -PassThru | Convert-DbcResult -Label "LCW_SQLPester_Check"
+
+$result | Write-DbaDataTable -SqlInstance "LCWSUNYONGENDB" -Database "SunucuYonetimGenel" -Table "SQLPester_TestResults" -Schema "dbo"
