299f0ad97684ff03308974eaa4668850a87136d9
ahmetsngr <88079221+ahmetsngr@users.noreply.github.com>
2023-03-07 15:12:54 +0300

    Add files via upload

---
+++ Pester - DB Insert.ps1
@@ -0,0 +1,12 @@
+﻿Import-Module PSFramework
+Import-Module dbatools
+
+$categories = "Instance","Server"
+$instance = "DEVCOMMONTR"
+##$instance = Get-Content -Path "C:\PSScripts\server-list.txt"
+
+$exluded_checks = "IdentityUsage","FKCKTrusted","DuplicateIndex","UnusedIndex","DisabledIndex","SymmetricKeyEncryptionLevel","AsymmetricKeySize","MaintenanceSolution","LogShipping", "QueryStoreEnabled","Agent","DatabaseCollation","InValidDatabaseOwner","SqlBrowserServiceAccount"
+
+$result = Invoke-DbcCheck -SqlInstance $instance -ComputerName $instance -AllChecks -ExcludeCheck $exluded_checks -PassThru | Convert-DbcResult -Label "LCW_SQLPester_Check"
+
+$result | Write-DbaDataTable -SqlInstance "LCWSUNYONGENDB" -Database "SunucuYonetimGenel" -Table "SQLPester_TestResults" -Schema "dbo"
