a10a0adc82546258c781fe26ba11fcc245645e42
ahmetsngr <88079221+ahmetsngr@users.noreply.github.com>
2023-03-17 09:47:17 +0300

    Add files via upload

---
+++ Pester Toplu Config Degistirme.ps1
@@ -0,0 +1,20 @@
+﻿
+
+Import-Module PSFramework
+Import-Module dbatools
+
+
+
+$categories = "Instance","Server"
+$instance = Get-Content -Path "C:\PSScripts\server-list.txt"
+$HostName=$instance
+
+$HostName= $instance.Replace("\AXAPTA","")
+
+Set-DbaPowerPlan -ComputerName $HostName -PowerPlan 'High Performance'
+Set-DbaSpConfigure -SqlInstance $instance -Name 'DefaultBackupCompression' -Value 1
+Set-DbaSpConfigure -SqlInstance $instance -Name 'RemoteDacConnectionsEnabled' -Value 1
+Set-DbaSpConfigure -SqlInstance $instance -Name 'MaxDegreeOfParallelism' -Value 1
+Set-DbaSpConfigure -SqlInstance $instance -Name 'OptimizeAdhocWorkloads' -Value 1
+
+
